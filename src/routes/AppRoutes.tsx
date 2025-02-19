b0e575d90834af6b5766aec871005ccee5cbb3d1
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-19 14:32:45 -0300

    alteraçõs locais na branch routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Home } from "../pages/Home";
 import { About } from "../pages/About";
 import { Messages } from "../pages/Messages";
 import { Services } from "../pages/Services";
+import { NotFound } from "../pages/NotFound";
 
 export function AppRoutes(){
   return(
@@ -11,6 +12,8 @@ export function AppRoutes(){
       <Route path="/about" element={<About />} />
       <Route path="/messages" element={<Messages />} />
       <Route path="/services" element={<Services />} />
+      
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
