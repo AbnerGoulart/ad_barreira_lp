9e0c74750cc0fcdbb623cd296954e4da9f18a051
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-19 14:20:05 -0300

    criação dos demais caminhos para as rotas de navegação da landing page

---
+++ src/pages/Messages.tsx
@@ -0,0 +1,7 @@
+export function Messages(){
+  return (
+    <>
+      <h1>Aqui traremos as Mensagens gravadas</h1>
+    </>
+  )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router";
 import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Messages } from "../pages/Messages";
+import { Services } from "../pages/Services";
 
 export function AppRoutes(){
   return(
     <Routes>
       <Route path="/" index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/services" element={<Services />} />
     </Routes>
   )
 }
