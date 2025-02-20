f52201e63ae3cffbc6b52c8548ad2b809aa7bcdb
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-20 16:33:33 -0300

    criação do componente Button

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,11 @@
 import bgImg from "../assets/re-web-header.jpeg";
 import bannerImg from "../assets/banner.png";
+import { Button } from "./Button";
 
 export function Banner() {
   return (
     <section 
-      className="w-full h-fit bg-cover bg-center bg-fixed flex justify-center items-center" 
+      className="w-full h-fit bg-cover bg-center bg-fixed flex flex-col justify-center items-center" 
       style={{ backgroundImage: `url(${bgImg})` }}
     >
       <img 
@@ -12,6 +13,9 @@ export function Banner() {
         alt="arte do evento" 
         className="max-w-full h-auto mx-auto" 
       />
+
+      <Button title="SAIBA MAIS"/>
+
     </section>
   );
 }
