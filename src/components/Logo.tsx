dde07eab950dc32d24bbd49e60293ca994f495bf
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-22 16:54:36 -0300

    reajuste das dimensões da logo do Header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,13 @@
-import logo from "../assets/utils/3-removebg-preview.png"
+import logo from "../assets/utils/3-removebg-preview.png";
 
 export function Logo() {
   return (
-    <div className="w-42 md:w-48 lg:w-48">
+    <div className="xs:w-24 sm:w-28 md:w-32 lg:w-40 xl:w-48 min-w-[100px] max-w-[250px]">
       <a href="/">
         <img
           src={logo}
           alt="Logo AD Barreira"
-          className="w-full h-auto object-contain rounded-lg"
+          className="w-full h-auto object-contain"
         />
       </a>
     </div>
