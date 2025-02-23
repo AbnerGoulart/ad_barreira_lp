a7f3e96bbf19ea7812d0ff4bed3c4a02e9316175
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-22 21:43:59 -0300

    correção de bug no SocialMedia

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -7,7 +7,10 @@ export function SocialMedia() {
   };
 
   return (
-    <aside className="absolute ml-2 left-2 sm:left-4 top-1/2 transform -translate-y-1/2 flex flex-col gap-4 sm:gap-6">
+    <aside 
+      className="absolute ml-2 left-2 sm:left-4 top-1/2 
+      transform -translate-y-1/2 flex flex-col gap-4 sm:gap-6"
+    >
       <ul className="flex flex-col gap-4 sm:gap-6">
         {socialMediaLinks.map(({ href, img, alt }, index) => (
           <li key={index}>
@@ -16,10 +19,9 @@ export function SocialMedia() {
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5, delay: 1 + index * 0.2 }}
               onClick={() => handleNavigate(href)}
-              className="cursor-pointer transition-transform duration-300 ease-in-out 
-              hover:scale-110 hover:brightness-125"
+              className="cursor-pointer transition-transform duration-300 ease-in-out"
             >
-              <img src={img} alt={alt} className="w-6 sm:w-8 md:w-10 lg:w-12" />
+              <img src={img} alt={alt} className="xs:w-6 sm:w-6 md:w-8 lg:w-10 hover:scale-110 hover:brightness-125" />
             </motion.button>
           </li>
         ))}
