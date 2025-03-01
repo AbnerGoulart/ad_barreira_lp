b1e0a665f132e2b90bf5dff94b6f227e288032c9
Abner Goulart <abnergoulart.10@gmail.com>
2025-02-28 23:10:26 -0300

    correção da estilização do AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -4,9 +4,9 @@ import { Footer } from "./Footer";
 
 export function AppLayout() {
   return (
-    <div className="w-screen min-h-screen bg-gray-100 text-gray-700 overflow-x-hidden">
+    <div className="w-full min-h-screen bg-gray-100 text-gray-700 overflow-x-hidden">
       <Header />
-      <main className="w-full md:w-auto mt-16 md:mt-16 overflow-x-hidden"> 
+      <main className="w-full md:w-auto mt-16 md:mt-16"> 
         <Outlet />
       </main>
       <Footer />
